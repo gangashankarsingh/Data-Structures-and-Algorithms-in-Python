9a470c25324e86f5aae2634cd9582e3e18cf1ae3
Gangashankar <bigdatamonk@gmail.com>
2020-05-30 21:21:46 +0530

    Algos

---
+++ Chapter1_PythonPrimer/R6.py
@@ -0,0 +1,7 @@
+def get_all_prev_squares(val):
+    return sum(x ** 2 for x in range(val) if x % 2 == 1)
+
+
+if __name__ == '__main__':
+    value = int(input("Enter a value until which you need the square"))
+    print(get_all_prev_squares(value))
